68ca0133c36298fe4be194e8c5382bb1f495379b
Nicolas Tessier <tessier.nicolas@outlook.fr>
2023-03-09 12:24:42 +0100

    adjust test

--- typescript/tests/game.spec.ts
+++ typescript/tests/game.spec.ts
@@ -2,14 +2,6 @@ import {GameRunner} from "../src/game-runner";
 import {ConsoleSpy} from "./ConsoleSpy";
 
 describe('The test environment', function() {
-    it("should pass", () => {
-        expect(true).toBe(true);
-    });
-
-    it("should access game", function () {
-        expect(GameRunner).toBeDefined()
-    });
-
     it("should test one player stop game", function () {
         const console = new ConsoleSpy();
         GameRunner.main(['Jean-Pierre'], console);
@@ -21,9 +13,16 @@ describe('The test environment', function() {
         GameRunner.main(['Jean-Pierre', 'Jean-Claude', 'Jean-Michel', 'Jean-Paul', 'Jean-Christophe', 'Jean-Baptiste', 'Jean-Philippe'], console);
         expect(console.Content).toContain("The game shouls contain 2 players minimum and 6 players maximum");
     });
-    it("should test four player and play game", function () {
+
+    it("should test two player and play game", function () {
         const console = new ConsoleSpy();
-        GameRunner.main(['Jean-Pierre', 'Jean-Claude', 'Jean-Michel', 'Jean-Paul'], console);
+        GameRunner.main(['Jean-Pierre', 'Jean-Claude'], console);
+        expect(console.Content).toContain("now has 6 Gold Coins.");
+    });
+
+    it("should test six player and play game", function () {
+        const console = new ConsoleSpy();
+        GameRunner.main(['Jean-Pierre', 'Jean-Claude', 'Jean-Michel', 'Jean-Paul', 'Jean-Christophe', 'Jean-Baptiste'], console);
         expect(console.Content).toContain("now has 6 Gold Coins.");
     });
 })
